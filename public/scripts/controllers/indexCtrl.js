17d7e64ca451b3ad3be29bbd20fc0c32db8ae797
David Wieczorek <blackratio@gmail.com>
2015-09-17 16:07:56 +0200

    Styles change

--- public/scripts/controllers/indexCtrl.js
+++ public/scripts/controllers/indexCtrl.js
@@ -9,6 +9,19 @@ function ($scope, $rootScope, $http, $timeout) {
 
 }]);
 
+directives.directive('anchorClick', function() {
+   return function(scope, element, attrs) {
+      $(element).click(function(event) {
+         event.preventDefault();
+         var target = this.hash;
+         var $target = $(target);
+
+         $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 50
+         }, 900);
+      });
+   };
+});
 
 controllers.controller('componentsCtrl', ['$scope', '$rootScope', '$http', '$timeout', '$location',
 function ($scope, $rootScope, $http, $timeout, $location) {

9229979ed86f6ef89df587f92de1a42a15080055
David Wieczorek <blackratio@gmail.com>
2015-10-12 12:08:24 +0200

    test

---
+++ public/scripts/services/services_index.js
@@ -0,0 +1,25 @@
+services.factory('userServices', ['$http', '$q', '$rootScope', '$timeout', '$stateParams', '$location', '$interval', '$window',
+function ($http, $q, $rootScope, $timeout, $stateParams, $location, $interval, $window){
+
+   return {
+
+      getUsers: function() {
+         var deferred = $q.defer();
+
+         var url = 'https://localhost:8080/user';
+
+         $http.get(url)
+         .success(function(data){
+            deferred.resolve(data);
+         })
+         .error(function(){
+            deferred.reject();
+         });
+         return deferred.promise;
+
+      }
+
+   };
+
+
+}]);

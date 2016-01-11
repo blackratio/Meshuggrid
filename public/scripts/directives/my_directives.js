d9d52a31ae0247d1966cbf0aa568c7817bad783b
blackratio <blackratio@gmail.com>
2016-01-11 09:37:43 +0100

    kg

--- public/scripts/directives/my_directives.js
+++ public/scripts/directives/my_directives.js
@@ -11,3 +11,19 @@ directives.directive('anchorClick', function() {
       });
    };
 });
+
+directives.directive('myDomDirective', function () {
+    return {
+        link: function ($scope, element, attrs) {
+            element.bind('click', function () {
+                element.html('You clicked me!');
+            });
+            element.bind('mouseenter', function () {
+                element.css('background-color', 'yellow');
+            });
+            element.bind('mouseleave', function () {
+                element.css('background-color', 'white');
+            });
+        }
+    };
+});
